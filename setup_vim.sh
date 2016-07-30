3af439ccc2434e600e52af4657bf29540190db1a
S1NH <hbyls@live.com>
2016-07-30 12:23:39 +0800

    修改了错误的路径，增加YouCompleteMe的安装

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-ln -s ./vim/vim-config/vimrc ~/.vimrc
+mv ~/.vimrc ~/.vimrc.bak
+ln -s ~/.vim/vim-config/vimrc ~/.vimrc
 vim +PluginInstall +qall
+
+sudo apt-get -y install build-essential cmake
+sudo apt-get -y install python-dev python3-dev
+
+cd ~/.vim/bundle/YouCompleteMe
+./install.py
 
 echo "dont forget to install / update YoucompleteMe"
 echo "https://github.com/j1z0/dotfiles.git"
